94d51c7b94242cf5a3a8838e70ef4288e792895f
shimantoneer <poddernakla@gmail.com>
2018-07-11 03:21:28 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.amazon.com/Pack-Plastic-White-Checkered-Tablecloths/dp/B0714QQMLM/ref=lp_901590_1_7?s=home-garden&ie=UTF8&qid=1530753729&sr=1-7");
+header("Location: https://www.amazon.com/gp/product/B076FTBSY7/ref=s9_acsd_newrz_hd_bw_bFenX_c_x_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-5&pf_rd_r=4DKV03YEZ4X2VVDNVQHY&pf_rd_t=101&pf_rd_p=6b6ff7bb-c02e-572a-aeda-c384e11d2edc&pf_rd_i=3731751");
 die();
 ?>
