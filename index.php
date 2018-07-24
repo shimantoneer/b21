a2977f86c99cf18ecf248aa6cfb5498f2495452a
shimantoneer <poddernakla@gmail.com>
2018-07-24 09:06:35 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.amazon.com/gp/product/B076FTBSY7/ref=s9_acsd_newrz_hd_bw_bFenX_c_x_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-5&pf_rd_r=4DKV03YEZ4X2VVDNVQHY&pf_rd_t=101&pf_rd_p=6b6ff7bb-c02e-572a-aeda-c384e11d2edc&pf_rd_i=3731751");
+header("Location: https://www.walmart.com/ip/Mainstays-Upholstered-Metal-Headboard-Multiple-Sizes/488974202");
 die();
 ?>
